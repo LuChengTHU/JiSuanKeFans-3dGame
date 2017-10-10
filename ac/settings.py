594d6a31e56f5ef8f84c80d329e9babc8dcb5c9b
lazycal <lazycal12@gmail.com>
2017-10-11 00:53:17 +0800

    add some lines to settings from dev branch

--- ac/settings.py
+++ ac/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
+    'api',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
