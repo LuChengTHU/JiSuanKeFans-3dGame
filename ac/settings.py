c0c44da83f8a270e6ce484512f739c27f84eb91f
YU_Jason <yuzhijingcheng1996@hotmail.com>
2017-10-10 11:43:14 +0800

    #11 Database password using environment variable

--- ac/settings.py
+++ ac/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db_ac',
         'USER': 'root',
-        'PASSWORD': '123456',
+        'PASSWORD': os.environ['MYSQL_ROOT_PASSWORD'],
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
