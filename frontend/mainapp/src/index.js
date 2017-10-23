6543ed3f6ebc74d4b8a50a9157d349a0a464eb8e
lazycal <lazycal12@gmail.com>
2017-10-23 16:08:00 +0800

    #28 Basic material-ui framework built

--- frontend/mainapp/src/index.js
+++ frontend/mainapp/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import App from './components/App.js';
+import DashBoard from './components/pages/DashBoard'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App>
+       <DashBoard />
+    </App>, 
+    document.getElementById('root'));
 registerServiceWorker();
