de8b547af7e3d06406b9ff9104947b7648a33357
YU_Jason <yuzhijingcheng1996@hotmail.com>
2017-10-25 10:51:42 +0800

    #26 fundamental framework for map editor added.

---
+++ frontend/mainapp/src/editor.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<MapEditor/>, document.getElementById('id'));
+registerServiceWorker();
