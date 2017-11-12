cf0822d061ed2626aaeb7044413ddc17cca86155
2015011276 <zhujh15@mails.tsinghua.edu.cn>
2017-11-12 13:55:51 +0800

    Build EditorGame for Editor page. #86

---
+++ frontend/mainapp/src/components/EditorGame.js
@@ -0,0 +1,160 @@
+import React, { Component, PropTypes, } from 'react';
+import React3 from 'react-three-renderer';
+
+import { Vector3, Euler, Geometry, DoubleSide, } from 'three';
+
+import * as THREE from 'three';
+
+import Player from './Player';
+import Bar from './Bar';
+import MapBlock from './MapBlock';
+import Monster from './Monster';
+import MouseInput from '../utils/MouseInputs';
+
+/**
+ * Our main class to display the game. This contains only view code! It's very
+ * easy to reason about
+ */
+
+export default class Game extends Component {
+
+    static propTypes = {
+        width: PropTypes.number.isRequired,
+        height: PropTypes.number.isRequired,
+        cameraPosition: PropTypes.instanceOf( Vector3 ).isRequired,
+        cameraRotation: PropTypes.instanceOf( Euler ).isRequired,
+        playerPosition: PropTypes.instanceOf( Vector3 ).isRequired,
+        playerRotation: PropTypes.instanceOf( Euler ).isRequired,
+    };
+
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        const {
+            width, 
+            height, 
+            
+            cameraPosition, 
+            cameraRotation, 
+            
+            mapBlocks, 
+            playerPosition, 
+            playerRotation, 
+            monsters, 
+            playerHp,
+            playerMaxHp, 
+            
+            targetPosition, 
+            knightMesh
+        } = this.props;
+
+		// return <div> width={ width }, height={ height }
+                    // lookAt={ lookAt.x } </div>
+
+		let ms = [];
+		let mbar = [];
+		for(let i = 0; i < monsters.length; ++i)
+			if(monsters[i].hp <= 0)
+			{
+				ms.push(null); mbar.push(null);
+			}
+			else
+			{
+				ms.push(<Monster position={monsters[i].position} rotation={monsters[i].rotation} monsterMesh={monsters[i].mesh}/>);
+				mbar.push(<Bar position={monsters[i].position} curValue={monsters[i].hp} maxValue={monsters[i].maxHp}/>);
+			}
+		
+		let target = null;
+		if(targetPosition)
+		{
+			target = <group
+				position={ targetPosition }
+			>
+				<mesh>
+					  <boxGeometry
+						width={1}
+						height={0.1}
+						depth={1}
+					  />
+					<meshLambertMaterial
+						color={0xffff00}
+					/>
+				</mesh>
+			</group>;
+		}
+
+		let ans = <React3
+            mainCamera="camera"
+            width={ width }
+            height={ height }
+            antialias
+        >
+            <module
+                ref="mouseInput"
+                descriptor={MouseInput}
+            />
+            <resources>
+                <texture
+                    resourceId="robotImage"
+                    url={ require( '../assets/sitepoint-robot-texture.jpg' ) }
+                    anisotropy={ 16 }
+                />
+                <meshPhongMaterial
+                    resourceId="robotTexture"
+                    side={ DoubleSide }
+                >
+                    <textureResource
+                        resourceId="robotImage"
+                    />
+                </meshPhongMaterial>
+                <texture
+                    resourceId="grassImage"
+                    url={ require( '../assets/grass.jpg' ) }
+                    anisotropy={ 16 }
+                />
+                <meshPhongMaterial
+                    resourceId="grassTexture"
+                    side={ DoubleSide }
+                >
+                    <textureResource
+                        resourceId="grassImage"
+                    />
+                </meshPhongMaterial>
+            </resources>
+            <scene ref={val => { this.sceneRef = val; }}>
+                <perspectiveCamera
+                    name="camera"
+                    ref="camera"
+                    fov={ 75 }
+                    aspect={ width / height }
+                    near={ 0.1 }
+                    far={ 1000 }
+                    position={ cameraPosition }
+                    rotation={ cameraRotation }
+                    lookAt={ lookAt }
+                />
+                <ambientLight
+                    color={ 0xdddddd }
+                />
+				{ mapBlocks }
+				<Bar
+					position = {playerPosition}
+					curValue = {playerHp}
+					maxValue = {playerMaxHp}
+				/>
+				{ ms }
+				{mbar}
+				{target}
+				<Player
+                    position = {playerPosition}
+                    rotation = {playerRotation}
+                    playerMesh = {knightMesh}
+                />
+            </scene>
+        </React3>;
+		return ans;
+    }
+
+}
