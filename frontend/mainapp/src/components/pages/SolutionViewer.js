65f7063ca1c99972b42f14ec9eee3c09fb78d065
YU_Jason <yuzhijingcheng1996@hotmail.com>
2017-11-22 11:24:39 +0800

    #90 SolutionViewer now supports running the code.

--- frontend/mainapp/src/components/pages/SolutionViewer.js
+++ frontend/mainapp/src/components/pages/SolutionViewer.js
@@ -7,6 +7,8 @@ import GameContainer from '../../containers/GameContainer'
 import BlocklyContainer from '../../containers/BlocklyContainer'
 import Button from 'material-ui/Button'
 import Grid from 'material-ui/Grid'
+import Logic from '../../logic/logic';
+import EnhancedInterpreter from '../EnhancedInterpreter';
 
 const SolutionViewer = createReactClass({
     getInitialState: function(){
@@ -22,8 +24,9 @@ const SolutionViewer = createReactClass({
             <Grid container spacing={25} justify='center'>
             <Grid item xs={12} sm={6} >
             <div id={'gameContainer'}>
-            <GameContainer/></div>
-            <Button onClick={() => this.loadSolution()}>Load</Button>
+            <GameContainer gameState={this.state.gameState} gameSetState={()=>{}}/></div>
+            <Button onClick={this.loadSolution}>Load</Button>
+            <Button onClick={this.run}>Run</Button>
             </Grid>
             <Grid item xs={12} sm={6}>
             <BlocklyContainer readOnly refCallback={(e)=>{this.blocklyContainer = e;}}/>
@@ -32,10 +35,20 @@ const SolutionViewer = createReactClass({
         );
     },
     loadSolution: function(){
-        fetch_map(this.solution.map.id).then(function(response){
-            window.Game.gameSetMap(window.map);
+        fetch_map(this.solution.map.id).then((response) => {
+            Logic.gameSetMap(window.map);
+            this.setState({gameState: "ready"});
+            this.blocklyContainer.highlightBlock('');
+            this.enhancedInterpreter = new EnhancedInterpreter(window.Game, this.blocklyContainer, this.gameSetState);
         });
         this.blocklyContainer.loadXmlText(this.solution.code);
+
+    },
+    run: function(){
+        window.Game.gameInit();
+        this.setState({gameState: "stepping"});
+        this.enhancedInterpreter.loadProgram(this.blocklyContainer.getCode());
+        this.enhancedInterpreter.step();
     }
 });
 
