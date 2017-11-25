68596a2e828c7c90eff3f7c0b8ebbbc3637e5170
于纪平 <yujp15@mails.tsinghua.edu.cn>
2017-11-25 15:29:05 +0800

    Remove useless editor.js #146

--- frontend/mainapp/src/editor.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<MapEditor/>, document.getElementById('id'));
-registerServiceWorker();
